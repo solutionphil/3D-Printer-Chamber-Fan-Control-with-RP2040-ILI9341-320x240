e9f92a6d750aa8219ed19fa0cb375006d09e5b49
solutionphil <125868581+solutionphil@users.noreply.github.com>
2025-01-21 00:31:46 +0100

    Update RP2040_ILI9341_Touch.h

--- TFT_eSPI/RP2040_ILI9341_Touch.h
+++ TFT_eSPI/RP2040_ILI9341_Touch.h
@@ -102,17 +102,23 @@
 // #define TFT_BACKLIGHT_ON HIGH  // Level to turn ON back-light (HIGH or LOW)
 
 // We must use hardware SPI, a minimum of 3 GPIO pins is needed.
-// Typical setup for the RP2040 is :
-//
-// Display SDO/MISO  to RP2040 pin D0 (or leave disconnected if not reading TFT)
-// Display LED       to RP2040 pin 3V3 or 5V
-// Display SCK       to RP2040 pin D2
-// Display SDI/MOSI  to RP2040 pin D3
-// Display DC (RS/AO)to RP2040 pin D18 (can use another pin if desired)
-// Display RESET     to RP2040 pin D19 (can use another pin if desired)
-// Display CS        to RP2040 pin D20 (can use another pin if desired, or GND, see below)
-// Display GND       to RP2040 pin GND (0V)
-// Display VCC       to RP2040 5V or 3.3V (5v if display has a 5V to 3.3V regulator fitted)
+// Typical setup for the RP2040 ZERO is :<br/>
+// Display PINS:<br/>
+// Display SDO/MISO  to RP2040 pin D0 (or leave disconnected if not reading TFT)<br/>
+// Display LED       to RP2040 pin 3V3 or 5V<br/>
+// Display SCK       to RP2040 pin D2<br/>
+// Display SDI/MOSI  to RP2040 pin D3<br/>
+// Display DC (RS/AO)to RP2040 pin D4 (can use another pin if desired)<br/>
+// Display RESET     to RP2040 pin D6 (can use another pin if desired)<br/>
+// Display CS        to RP2040 pin D1 (can use another pin if desired, or GND, see below)<br/>
+// Display GND       to RP2040 pin GND (0V)<br/>
+// Display VCC       to RP2040 5V or 3.3V (5v if display has a 5V to 3.3V regulator fitted)<br/>
+// Touch Pins:<br/>
+// T_IRQ             Not Connected<br/>
+// T_D0  						to RP2040 pin D0 (must be same like Display SDO/MISO)<br/>
+// T_DIN  						to RP2040 pin D3 (must be the same like Display SDI/MOSI)<br/>
+// T_CS  						to RP2040 pin D5 (can use another pin if desired, or GND, see below)<br/>
+// T_CLK  						to RP2040 pin D1 (must be the same like Display CS)<br/><br/>
 //
 // The DC (Data Command) pin may be labelled AO or RS (Register Select)
 //
